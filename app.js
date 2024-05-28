806ec8673f6132e4d6efc50ddb2a0731967c58ed
s0nderz <only94-@hotmail.com>
2024-05-28 12:42:22 +0200

    updated js with addeventlistener instead

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+window.onload=function() {
+
+
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f",
 "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
 "$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let generatePasswordBtn = document.getElementById("generatePasswordBtn")
 
 function generateThePassword() {
     for(let i = 0; i < 15; i++) {
@@ -18,11 +22,12 @@ function clearText() {
     document.getElementById("password2").textContent = ""
 }
 
-function generateThePasswordAndClearText() {
+generatePasswordBtn.addEventListener("click", function(){
     if(document.getElementById("password1").textContent === "" && document.getElementById("password2").textContent === "") {
-    generateThePassword()
+        generateThePassword()
     } else {
-    clearText()
-    generateThePassword()
+        clearText()
+        generateThePassword()
     }
+})
 }
